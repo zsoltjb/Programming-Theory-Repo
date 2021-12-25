e1d276265eb0933f6348394f0ed061a0541b31a1
zsoltjb <zjb@digitalrifle.com>
2021-12-25 22:52:00 +0000

    Options scene concept design

---
+++ Programming-Theory/Assets/Scripts/Planet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Base class for all Planet
+public abstract class Planet : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
